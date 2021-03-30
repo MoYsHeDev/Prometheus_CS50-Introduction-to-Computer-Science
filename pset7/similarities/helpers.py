52564fd460f83d2745b922354c4cf9e4c501c97f
Michail Frolov <64253494+MoYsHeDev@users.noreply.github.com>
2021-03-30 19:00:06 +0300

    1st task of the 7th week "Similarities"
    1. Create a program that looks for similarities in two
    files.
    
    2. Create a website that highlights the similarities
    in the two files, as shown below.

---
+++ pset7/similarities/helpers.py
@@ -0,0 +1,19 @@
+def lines(a, b) -> 'str':
+    """Return lines in both a and b"""
+
+    # TODO
+    return []
+    
+
+def sentences(a, b):
+    """Return sentences in both a and b"""
+
+    # TODO
+    return []
+
+
+def substrings(a, b, n):
+    """Return substrings of length n in both a and b"""
+
+    # TODO
+    return []
