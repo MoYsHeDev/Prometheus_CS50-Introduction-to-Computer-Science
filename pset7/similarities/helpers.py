bb368f2ec9ffa28e67e4fb46f08bf040c9de78f3
Michail Frolov <64253494+MoYsHeDev@users.noreply.github.com>
2021-03-31 15:53:53 +0300

    Implemented 3 functions according to the conditions of the task:  "Specification / helpers.py"

--- pset7/similarities/helpers.py
+++ pset7/similarities/helpers.py
@@ -1,38 +1,37 @@
 from nltk.tokenize import sent_tokenize
 
 
-def lines(a: str, b: str) -> set & set:
+def lines(a: str, b: str):
     """Return lines in both a and b"""
 
     a_lines = set(a.splitlines())
     b_lines = set(b.splitlines())
 
-    return a_lines and b_lines
+    return a_lines & b_lines
     
 
-def sentences(a: str, b: str) -> set & set:
+def sentences(a: str, b: str):
     """Return sentences in both a and b"""
 
     a_sentences = set(sent_tokenize(a))
     b_sentences = set(sent_tokenize(b))
 
-    return a_sentences and b_sentences
+    return a_sentences & b_sentences
 
 
-def substrings(a: str, b: str, n: int) -> set & set:
+def substring_tokenizer(string: str, length: int) -> str:
+    """Return substrings of length "length" in both "string" """
+    substrings_str = []
+
+    for i in range(len(string) - length + 1):
+        substrings_str.append(string[i:i + length])
+    return substrings_str
+
+
+def substrings(a: str, b: str, n: int):
     """Return substrings of length n in both a and b"""
-
-    def substring_tokenizer(string: str, length: int) -> list:
-        """Return substrings of length "length" in both "string" """
-        substrings_str = []
-
-        for i in range(0, len(string) - length):
-            temp = string[i:i + length]
-            substrings_str.append(temp)
-
-        return substrings_str
 
     a_substrings = set(substring_tokenizer(a, n))
     b_substrings = set(substring_tokenizer(b, n))
 
-    return a_substrings and b_substrings
+    return a_substrings & b_substrings
