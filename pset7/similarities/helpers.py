44c52167bee5492e9e074ae58164cc9135e42d3c
Michail Frolov <64253494+MoYsHeDev@users.noreply.github.com>
2021-03-31 13:23:26 +0300

    Implemented 3 functions according to the conditions of the task:  "Specification / helpers.py"

--- pset7/similarities/helpers.py
+++ pset7/similarities/helpers.py
@@ -1,19 +1,40 @@
-def lines(a, b) -> 'str':
+from nltk.tokenize import sent_tokenize
+
+
+def lines(a: str, b: str) -> set & set:
     """Return lines in both a and b"""
 
-    # TODO
-    return []
+    a_lines = set(a.splitlines())
+    b_lines = set(b.splitlines())
+
+    return a_lines and b_lines
     
 
-def sentences(a, b):
+def sentences(a: str, b: str) -> set & set:
     """Return sentences in both a and b"""
 
-    # TODO
-    return []
+    a_sentences = set(sent_tokenize(a))
+    b_sentences = set(sent_tokenize(b))
+
+    return a_sentences and b_sentences
 
 
-def substrings(a, b, n):
+def substrings(a: str, b: str, n: int) -> set & set:
     """Return substrings of length n in both a and b"""
 
-    # TODO
-    return []
+    def substring_tokenizer(string: str, length: int) -> list:
+        """Return substrings of length "length" in both "string" """
+        substrings_str = []
+
+        for i in range(0, len(string) - length):
+            temp = string[i:i + length]
+            substrings_str.append(temp)
+
+        return substrings_str
+
+    a_substrings = set(substring_tokenizer(a, n))
+    b_substrings = set(substring_tokenizer(b, n))
+
+    return a_substrings and b_substrings
+
+
